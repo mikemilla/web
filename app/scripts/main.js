e78d5b8662630d0d6475f2848297eeced3fa52cc
Michael Miller <mail@mikemilla.com>
2017-08-29 23:50:38 -0400

    Code Cleanup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,54 +5,36 @@ $(window).load(function() {
     const primaryColor = '#108FFF';
     const transitionValues = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
 
-    // Animate body in
+    // Elements
     const body = $('body');
-    body.css('margin-top', 0);
-    body.css('opacity', 1);
-
-    // Transition completion listener
-    // body.one(transitionValues, function(event) {
-    //     body.css('background', primaryColor);
-    // });
-
     const header = $('header');
     const headerImage = $('header .image');
     const headerTitles = $('header .titles');
 
-    const animatedListItems = $('.animated-li').map(function() {
-        return this;
-    }).get();
-
+    // Perform animations
+    body.css('margin-top', 0);
+    body.css('opacity', 1);
     headerTitles.css('opacity', 1);
     headerImage.css('opacity', 1);
-
     header.addClass('shown');
 
-    // const duration = listItemAtIndex.css('transition-duration');
-
+    // Listen to header animation ending
     header.one(transitionValues, function(event) {
         header.unbind(transitionValues);
 
         // Set background color
         body.css('background', primaryColor);
 
-        // Get specific animated list items
-        const animatedListItems = $('li').map(function() {
-            return this;
-        }).get();
-
         // Loop list items in array
-        for (i = 0; i < animatedListItems.length; i++) {
-
-            // Item at index
-            const listItemAtIndex = $(animatedListItems[i])
+        $('.grid ul li').each(function(index, element) {
+            const listItemAtIndex = $(this)
             const duration = Math.round(parseFloat(listItemAtIndex.css('transition-duration')) * 1000)
 
             // Delay items
             setTimeout(function() { 
                 listItemAtIndex.css('opacity', 1);
                 listItemAtIndex.css('margin-top', 0);
-            }, (duration / 6) * i);
-        }
+            }, (duration / 6) * index);
+        });
     });
 });
