8b8e3c41efb4ba82c41d6435a650c08a47839c58
Mike Miller <mike@mikemiller.design>
2026-02-05 10:23:35 -0800

    Profile update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,9 +6,12 @@ export function Header() {
       <div className="keylines">
         <div className="titles">
           <h1>Mike Miller</h1>
-          <h2>Design Engineer</h2>
+          <h2>Staff Design Engineer</h2>
           <p>
             I ship product—from pixels to production. Tastefully design products and engineer them to life. Any stack, any platform.
+            <br />
+            <br />
+            {new Date().getFullYear() - 2012}+ years experience working with every mobile platform, most frontend frameworks, backend & infra and even custom AI inference & MCP servers.
             <br />
             <br />
             Scroll down to see some work!
@@ -25,7 +28,7 @@ export function Header() {
         </div>
         <div className="image">
           <Image
-            src="/assets/images/profile_1.png"
+            src="/assets/images/profile_2.png"
             alt="Mike Miller"
             width={590}
             height={600}
